11d05e9fd13c664d421fabd35804cbf942229966
Alexander Lenhardt <alexander.lenhardt@soundcloud.com>
2016-09-02 14:33:39 +0200

    sqlcipher project

---
+++ cmake/projects/sqlcipher/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_configuration_types)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+        PACKAGE_NAME
+        SQLCipher
+        VERSION
+        "3.4.0"
+        URL
+        "https://github.com/sqlcipher/sqlcipher/archive/v3.4.0.tar.gz"
+        SHA1
+        9380db7bd1d304b9db59c5d9c26c449ee57bfe01
+)
+
+hunter_configuration_types(SQLCipher CONFIGURATION_TYPES Release)
+hunter_pick_scheme(DEFAULT url_sha1_sqlcipher)
+hunter_cacheable(SQLCipher)
+hunter_download(
+        PACKAGE_NAME SQLCipher
+        PACKAGE_INTERNAL_DEPS_ID "2"
+        PACKAGE_UNRELOCATABLE_TEXT_FILES
+        "lib/libsqlcipher.la"
+        "lib/pkgconfig/sqlcipher.pc"
+)
